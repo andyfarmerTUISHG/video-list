4945f7f3de639cfab348e7d076559e9ac5d2f117
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-02 20:01:51 +0000

    Convert Video to Stateful Class Component

--- react-video-app/src/components/Video.js
+++ react-video-app/src/components/Video.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-function Video(props) {
-  const { name, mediaType, genre } = props.video;
-  return (
-    <div className="card card-body mb-3">
-      <h2>Name: {name}</h2>
-      <ul className="list-group">
-        <li className="list-group-item">Media: {mediaType}</li>
-        <li className="list-group-item">Style: {genre}</li>
-      </ul>
-    </div>
-  );
+class Video extends Component {
+  render() {
+    const { name, mediaType, genre } = this.props.video;
+    return (
+      <div className="card card-body mb-3">
+        <h2>Name: {name}</h2>
+        <ul className="list-group">
+          <li className="list-group-item">Media: {mediaType}</li>
+          <li className="list-group-item">Style: {genre}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-Video.prototype = {
-  name: PropTypes.string.isRequired,
-  mediaType: PropTypes.string.isRequired
+Video.propTypes = {
+  video: PropTypes.object.isRequired
 };
 export default Video;
