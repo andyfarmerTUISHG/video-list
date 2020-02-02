cfc050e23778d0d1fa94b97d62606d9ed2a16936
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-02 15:35:58 +0000

    Simplified Videos.js State - Removed the constructor method as not required at this time

--- react-video-app/src/components/Videos.js
+++ react-video-app/src/components/Videos.js
@@ -2,37 +2,34 @@ import React, { Component } from "react";
 import Video from "./Video";
 
 class Videos extends Component {
-  constructor() {
-    super();
-    this.state = {
-      videos: [
-        {
-          id: 1,
-          name: "Aliens",
-          genre: "SCI-FI",
-          mediaType: "DVD"
-        },
-        {
-          id: 2,
-          name: "Alien",
-          genre: "SCI-FI",
-          mediaType: "DVD"
-        },
-        {
-          id: 3,
-          name: "Deadpool",
-          genre: "ACTION",
-          mediaType: "Blu-Ray"
-        },
-        {
-          id: 4,
-          name: "Deadpool 2",
-          genre: "ACTION",
-          mediaType: "Blu-Ray"
-        }
-      ]
-    };
-  }
+  state = {
+    videos: [
+      {
+        id: 1,
+        name: "Aliens",
+        genre: "SCI-FI",
+        mediaType: "DVD"
+      },
+      {
+        id: 2,
+        name: "Alien",
+        genre: "SCI-FI",
+        mediaType: "DVD"
+      },
+      {
+        id: 3,
+        name: "Deadpool",
+        genre: "ACTION",
+        mediaType: "Blu-Ray"
+      },
+      {
+        id: 4,
+        name: "Deadpool 2",
+        genre: "ACTION",
+        mediaType: "Blu-Ray"
+      }
+    ]
+  };
   render() {
     const { videos } = this.state;
     return (
