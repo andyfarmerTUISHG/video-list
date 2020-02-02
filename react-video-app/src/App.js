1a42f965aea68770c8fbcbc52f27d3612d585311
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-02 12:25:37 +0000

    Added Boortstrap to the App

--- react-video-app/src/App.js
+++ react-video-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Video from "./components/Video";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
