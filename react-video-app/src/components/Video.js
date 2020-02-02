ed8c235ceb4beebf5e94191885455ec14456f738
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-02 20:33:13 +0000

    Video.js - Show how to pass this  from a component - Show how to pass variables

--- react-video-app/src/components/Video.js
+++ react-video-app/src/components/Video.js
@@ -3,17 +3,20 @@ import PropTypes from "prop-types";
 
 class Video extends Component {
   state = {};
-  onShowClick = () => {
-    console.log(`in on show clicks ${this.state}`);
+  onShowClick = (id, name, e) => {
+    console.log(`in on show clicks ${id} - ${name}`);
   };
 
   render() {
-    const { name, mediaType, genre } = this.props.video;
+    const { id, name, mediaType, genre } = this.props.video;
     return (
       <div className="card card-body mb-3">
         <h2>
           Name: {name}
-          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          <i
+            onClick={this.onShowClick.bind(this, id, name)}
+            className="fas fa-sort-down"
+          />
         </h2>
         <ul className="list-group">
           <li className="list-group-item">Media: {mediaType}</li>
