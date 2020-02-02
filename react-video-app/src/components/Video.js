fcf84eb2a471d66fc861968f3f71c83b1e6eb62c
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-02 20:28:01 +0000

    Video.js - define an empty state - Add event to down arrow so we can hide list group - Create basic function that has  access to 'this' keyword

--- react-video-app/src/components/Video.js
+++ react-video-app/src/components/Video.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Video extends Component {
+  state = {};
+  onShowClick = () => {
+    console.log(`in on show clicks ${this.state}`);
+  };
+
   render() {
     const { name, mediaType, genre } = this.props.video;
     return (
       <div className="card card-body mb-3">
-        <h2>Name: {name}</h2>
+        <h2>
+          Name: {name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h2>
         <ul className="list-group">
           <li className="list-group-item">Media: {mediaType}</li>
           <li className="list-group-item">Style: {genre}</li>
