9e17b1e225c35e4ec6314e7dc0dc4bd7bd9e97f7
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-14 17:44:45 +0000

    Added Tests

--- react-video-app/src/App.test.js
+++ react-video-app/src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Video Manager Title", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Video Manager/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders intro", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Welcome to your custom media manager/i);
   expect(linkElement).toBeInTheDocument();
 });
