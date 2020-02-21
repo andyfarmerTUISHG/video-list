1db774caf16107aebb847d02c8b0eb6212f788a1
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-02-21 22:37:34 +0000

    Redux Pt2 Connecting Components & Dispatching Actions Videos.js - Removed State and moved to videoReducer videoReducer.js - Added vides to intial state actions/types.js - defined  action types in seperate file for video reducer for redux - defined GET_VIDEOS as a string videoReducer.js -import actions - use action in swithc statemnt - for short term return current state Videos.js - Connect component state with Redux Store via connect - Define PropTypes for videos and getVideos - Define Map functions StateToProps and DispatchToProps - Update export videoReducer.js - Increased the initial state - fixed spelling error

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,42 +1,20 @@
 import React, { Component } from "react";
 import Video from "./Video";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { GET_VIDEOS } from "../actions/types";
 
 class Videos extends Component {
-  state = {
-    videos: [
-      {
-        id: 1,
-        name: "Aliens",
-        genre: "SCI-FI",
-        mediaType: "DVD"
-      },
-      {
-        id: 2,
-        name: "Alien",
-        genre: "SCI-FI",
-        mediaType: "DVD"
-      },
-      {
-        id: 3,
-        name: "Deadpool",
-        genre: "ACTION",
-        mediaType: "Blu-Ray"
-      },
-      {
-        id: 4,
-        name: "Deadpool 2",
-        genre: "ACTION",
-        mediaType: "Blu-Ray"
-      }
-    ]
-  };
+  componentDidMount() {
+    this.props.getVideos();
+  }
   render() {
     // Access to whatever the provider gives us, in this case, the whole state
-    const { videos } = this.state;
+    const { videos } = this.props;
     return (
       <React.Fragment>
         <h2 className="display-4 mb-2">
-          <span className="text-danger">Video</span>List
+          <span className="text-danger">Video</span> List
         </h2>
         {videos.map(video => (
           <Video key={video.id} video={video} />
@@ -46,4 +24,20 @@ class Videos extends Component {
   }
 }
 
-export default Videos;
+Videos.propTypes = {
+  videos: PropTypes.array.isRequired,
+  getVideos: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  videos: state.video.videos
+});
+
+const mapDispatchToProps = dispatch => ({
+  getVideos: () =>
+    dispatch({
+      type: GET_VIDEOS
+    })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Videos);
--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -1,7 +1,46 @@
-const initialSate = {};
+import { GET_VIDEOS } from "../actions/types";
 
-export default function(state = initialSate, action) {
+const initialState = {
+  videos: [
+    {
+      id: 1,
+      name: "Aliens",
+      genre: "SCI-FI",
+      mediaType: "DVD"
+    },
+    {
+      id: 2,
+      name: "Alien",
+      genre: "SCI-FI",
+      mediaType: "DVD"
+    },
+    {
+      id: 3,
+      name: "Deadpool",
+      genre: "ACTION",
+      mediaType: "Blu-Ray"
+    },
+    {
+      id: 4,
+      name: "Deadpool 2",
+      genre: "ACTION",
+      mediaType: "Blu-Ray"
+    },
+    {
+      id: 5,
+      name: "Get Out",
+      genre: "Horror",
+      mediaType: "Blu-Ray"
+    }
+  ]
+};
+
+export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_VIDEOS:
+      return {
+        ...state
+      };
     default:
       return state;
   }
