e855af82c075caf1d96f809476666faa40fef3e4
Farmer, Andy <Andy.Farmer@specialistholidays.com>
2020-03-01 10:17:53 +0000

    Pull List from my-json-server videoActions - Use axios to get response from my-json-server - user dispatch to send payload of res.data to videoReducer VideoReducer.js - Remove initial state object - Update GetVideo Action to to add payload to state

--- src/actions/videosActions.js
+++ src/actions/videosActions.js
@@ -1,9 +1,14 @@
 import { GET_VIDEOS, ADD_VIDEO, DELETE_VIDEO } from "../actions/types";
+import Axios from "axios";
 
-export const getVideos = () => {
-  return {
-    type: GET_VIDEOS
-  };
+export const getVideos = () => async dispatch => {
+  const res = await Axios.get(
+    "https://my-json-server.typicode.com/andyfarmerTUISHG/video-json/videos/"
+  );
+  dispatch({
+    type: GET_VIDEOS,
+    payload: res.data
+  });
 };
 export const addVideo = video => {
   return {
--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -1,45 +1,15 @@
 import { GET_VIDEOS, ADD_VIDEO, DELETE_VIDEO } from "../actions/types";
 
 const initialState = {
-  videos: [
-    {
-      id: 1,
-      name: "Aliens",
-      genre: "SCI-FI",
-      mediaType: "DVD"
-    },
-    {
-      id: 2,
-      name: "Alien",
-      genre: "SCI-FI",
-      mediaType: "DVD"
-    },
-    {
-      id: 3,
-      name: "Deadpool",
-      genre: "ACTION",
-      mediaType: "Blu-Ray"
-    },
-    {
-      id: 4,
-      name: "Deadpool 2",
-      genre: "ACTION",
-      mediaType: "Blu-Ray"
-    },
-    {
-      id: 5,
-      name: "Get Out",
-      genre: "Horror",
-      mediaType: "Blu-Ray"
-    }
-  ]
+  videos: []
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_VIDEOS:
       return {
-        ...state
+        ...state,
+        videos: action.payload
       };
     case ADD_VIDEO:
       return {
